6dfc039ae6b24e5cb4c97ed5a77fefd223a66bee
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-09-23 13:38:22 +0530

    update array

---
+++ function and filter/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Function and Filter</title>
+</head>
+
+
+<body>
+<?php
+  $books = [
+  [
+     'name' => 'The Martian',
+     'author' => 'Andy Weir',
+     'releaseYear' => 2011,
+     'purchaseUrl' => 'https://example.com/the-martian'
+  ],
+  [
+    'name' => 'The Martian',
+    'author' => 'Andy Weir',
+    'releaseYear' => 2011,
+    'purchaseUrl' => 'https://example.com/artemis'
+  ]
+  ];
+
+  function filterByAuther($book){
+      return 'filter';
+
+   }
+ ?>
+
+ <ul>
+     <?php foreach ($books as $book): ?>
+         <?php if ($book['author'] == 'Andy Weir'): ?>
+             <li>
+                 <a href="<?= $book['purchaseUrl'] ?>">
+                     <?= $book['name']; ?> (<?= $book['releaseYear'] ?>)
+                 </a>
+             </li>
+         <?php endif; ?>
+     <?php endforeach; ?>
+ </ul>
+ <p>
+     <?php echo
+         filterByAuther();         //"php echo" = "="
+     ?>
+ </p>
+
+</body>
+</html>
