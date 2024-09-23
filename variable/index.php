d2e8f2c45c62353e917d0381a5c2ad94a124a0ae
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-09-23 14:35:32 +0530

    update variable sampale project

---
+++ variable/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Variable</title>
+</head>
+<body>
+<h1>
+<?php
+    $name = "kamala"; // String
+    $age = 25;        // Integer
+    $isStudent = true; // Boolean
+
+    echo $name;  // Outputs: kamala
+    echo $age;   // Outputs: 25
+    echo $isStudent; // Outputs: 1
+    ?>
+</h1>
+</body>
+</html>
